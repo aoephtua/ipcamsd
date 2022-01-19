72078853d757a0cfe9f130c66c857dc11d0af646
aoephtua <aoephtua@mailo.com>
2022-01-19 09:27:46 +0100

    Increases tool version to 0.2.0

--- src/cli.js
+++ src/cli.js
@@ -46,7 +46,7 @@ function addCommand(name, isDefault, cbAddOptions) {
 }
 
 program
-    .version('0.1.10', '-v, --version');
+    .version('0.2.0', '-v, --version');
 
 addCommand('fetch', true, (command) => {
     command
